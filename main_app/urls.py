4f9750c174ba222e705580e06caf1d6dc5e69164
desertlp <lorapaine1@gmail.com>
2020-08-05 16:29:36 -0700

    cities and post routes/views/templates started

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,14 +9,15 @@ urlpatterns = [
         # Render a blank template
     # City Index
     path('cities/', views.cities, name="cities"),
+    path('city/<int:city_id>/', views.city, name="city"),
+    path('city/<int:city_id>/new_post', views.new_post, name="new_post"),
+    
+    path('post/new/', views.new_post, name='new_post'),
+        # this path should be from the city page 
+
     path('post/<int:post_id>/', views.post, name='post'),
     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
-        # city.posts
-        # event listener to display a city to the right on click? 
-    # Add Post Page
-        # City automatically applied
-    # Post Show Page
-    # Sign Up
+
     path('accounts/signup', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit', views.edit_profile, name='edit_profile'),
