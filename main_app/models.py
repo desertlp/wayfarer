b8825396402e3b06fdf9b18ef2346f99834f93a3
desertlp <lorapaine1@gmail.com>
2020-08-06 11:10:24 -0700

    migrated, new image upload path'

--- main_app/models.py
+++ main_app/models.py
@@ -25,15 +25,6 @@ class Profile(models.Model):
             img.save(self.image.path)
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
-
 class City(models.Model): 
     name = models.CharField(max_length=100, blank=False)
     latitude = models.DecimalField(blank=True, max_digits=10, decimal_places=6)
